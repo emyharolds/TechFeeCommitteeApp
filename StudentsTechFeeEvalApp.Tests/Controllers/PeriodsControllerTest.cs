e3f0c067f5b2d32167e54e211d8875ae8595b4e4
Harold Okafor <okaforharold@gmail.com>
2018-05-08 01:13:44 -0500

    add unit tests

---
+++ StudentsTechFeeEvalApp.Tests/Controllers/PeriodsControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentsTechFeeEvalApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsTechFeeEvalApp.Tests.Controllers
+{
+    [TestClass]
+    class PeriodsControllerTest
+    {
+        [TestMethod]
+        public void TestIndexView()
+        {
+            var controller = new PeriodsController();
+            var result = controller.Index();
+            Assert.AreEqual("Index", result);
+        }
+    }
+}
